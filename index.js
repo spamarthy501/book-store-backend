fc89fcc9d3c639356dc9a6bbe2c9dc25c858e37c
spamarthy501 <spamarthy501@gmail.com>
2025-03-13 14:39:15 -0400

    added vercel link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 //middleware
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", https://book-store-frontend-lac.vercel.app/],
     credentials: true
 }));
 
